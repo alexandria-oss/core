3699f51b427a5595053f4918b334f8582b1db6ff
Alonso R <luis.alonso.16@hotmail.com>
2020-05-20 12:38:56 -0600

    Update global consts

--- global.go
+++ global.go
@@ -1,13 +1,20 @@
 package core
 
-// RFC3339Micro Date only RFC format
-const RFC3339Micro = "2006-01-02"
-
-// PublicAPI Public API URI
-const PublicAPI = "/v1"
-
-// PrivateAPI Private API URI
-const PrivateAPI = "/v1/private"
-
-// AdminAPI Admin API URI
-const AdminAPI = "/v1/admin"
+const (
+	// RFC3339Micro Date only RFC format
+	RFC3339Micro = "2006-01-02"
+	// PublicAPI Public API URI
+	PublicAPI = "/v1"
+	// PrivateAPI Private API URI
+	PrivateAPI = "/v1/private"
+	// AdminAPI Admin API URI
+	AdminAPI = "/v1/admin"
+	// RoleRoot Superuser role
+	RoleRoot = "ROLE_ROOT"
+	// RoleAdmin Administrator role
+	RoleAdmin = "ROLE_ADMIN"
+	// RoleUser Default role
+	RoleUser = "ROLE_USER"
+	// LocaleDefault Locale default value
+	LocaleDefault = "EN-US"
+)
